2704f7759e4da3bc6e51a10a9d2c51506587c6b1
Ilya V. Schurov <ilya@schurov.com>
2021-03-29 20:04:40 +0300

    Added echo.

--- demo_app.py
+++ demo_app.py
@@ -2,12 +2,13 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import numpy as np
 
-st.title("Hello, World!")
-"""
-This is a test.
-"""
-x = np.linspace(0, 10, 500)
-fig = plt.figure()
-plt.plot(x, np.sin(x))
-plt.ylim(-2, 2)
-st.pyplot(fig)
+with st.echo(code_location='below'):
+    st.title("Hello, World!")
+    """
+    This is a test.
+    """
+    x = np.linspace(0, 10, 500)
+    fig = plt.figure()
+    plt.plot(x, np.sin(x))
+    plt.ylim(-2, 2)
+    st.pyplot(fig)
